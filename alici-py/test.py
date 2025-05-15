0d36902e043650465989643a66ef4279d9d1f732
matteusnascimento <81870400+matteusnascimento@users.noreply.github.com>
2025-05-15 17:27:07 -0300

    Delete alici-py directory

--- alici-py/test.py
+++
@@ -1,3 +0,0 @@
-import psycopg2
-conn = psycopg2.connect("postgresql://neondb_owner:npg_YA5DVX0fvWrg@ep-frosty-shape-a8fhb2m2-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
-print("Conexão ok!")
